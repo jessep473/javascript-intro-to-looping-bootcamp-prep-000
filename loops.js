1cf47fd0f7e57954feb563c96e6f4216339febd2
Jesse Peterson <jessep473@gmail.com>
2018-10-17 12:42:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,26 +1,9 @@
-function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
-  }
-  return array;
-}
- function whileLoop(n) {
-  let countdown = n;
-     while (countdown > 0) {
-      console.log(--countdown)
+function countdown(num) {
+  for (i = 0; i < num; i++ ) 
+    if (i === 0) {
+      console.log('Im Done');
     }
-    return "done";
-  }
- function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-   do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
+    else {
+      console.log('Counted' + i + 'times');
+    }
 }
